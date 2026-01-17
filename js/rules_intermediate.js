506b21f1b14e7c9aeb1b2c28e92aefe3f5e60d61
二叉树树 <acofork@qq.com>
2026-01-17 19:01:02 +0800

    Add short link: yEHvGLV

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2746,5 +2746,9 @@ window.RULES_INTERMEDIATE = {
     "/RY2C7hO": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/yEHvGLV": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
