806164c8cb9e7eb90ba6427f13b398bc237e4b74
二叉树树 <acofork@qq.com>
2026-01-17 18:57:31 +0800

    Add short link: 9FJQUGT

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2670,5 +2670,9 @@ window.RULES_INTERMEDIATE = {
     "/MEsrCq2": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/9FJQUGT": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
