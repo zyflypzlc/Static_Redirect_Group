b105f8d2ae0dc8439be65b7550ce358542f1757d
二叉树树 <acofork@qq.com>
2026-01-17 19:01:04 +0800

    Add short link: CMtxYRS

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2750,5 +2750,9 @@ window.RULES_INTERMEDIATE = {
     "/yEHvGLV": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/CMtxYRS": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
