96502751a8366d47c0f446ba9c172ba7f2d60b6b
二叉树树 <acofork@qq.com>
2026-01-17 18:57:08 +0800

    Add short link: HbraI5u

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2598,5 +2598,9 @@ window.RULES_INTERMEDIATE = {
     "/9qVdfRl": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/HbraI5u": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
