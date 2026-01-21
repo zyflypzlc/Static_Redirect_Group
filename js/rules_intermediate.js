e87f56bdc98344d910857051e0510655aba6d3af
zyflypzlc <47585716+zyflypzlc@users.noreply.github.com>
2026-01-21 14:48:44 +0800

    Update rules_intermediate.js

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -3,8 +3,8 @@ window.RULES_INTERMEDIATE = {
         "url": "https://www.baidu.com",
         "expired_at": "2026-01-22T03:22:00.000Z"
     },
-    "/baiduu": {
-        "url": "https://baidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.combaidu.com",
+    "/qq": {
+        "url": "https://qq.com",
         "expired_at": "2026-01-22T13:47:00.000Z"
     }
 };
